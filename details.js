3c064f5b58a24edc130a1ad448620b8d35702b16
Paulo <paulorzzbj@gmail.com>
2026-03-09 13:24:44 +0100

    search

--- details.js
+++ details.js
@@ -1,29 +1,52 @@
+import { arrowLeft, chevronLeft, chevronRight, pokeballBig } from "./icons.js"
+
+const pageSearch = window.location.search;
+console.log(pageSearch);
+const params = new URLSearchParams(pageSearch);
+console.log(params);
+let id = params.get("id")
+console.log(id)
+
+const mainDom = document.querySelector("main")
+const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+
 function fetchPokemon() {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then(response => response.json())
         .then((data) => {
             displayPokemonDetails(data)
+            fetchSpecies(data)
     })
+}
+
+async function fetchSpecies() {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+        .then(response => response.json())
+        const {flavor_text_entries} = 
 }
 
 function displayPokemonDetails(data) {
     const results = data.results
     const pokemonString = results.map((result) => {
-        const {name, url, stats, types, height, weight, abilities} = result
+        const {name, url, types=[], weight, height, abilities=[], stats=[]} = result
         const dexNumber = getPokemonId(url)
         const capitalizedName =
             name.charAt(0).toUpperCase()
             + name.slice(1)
         return /*html*/ `
-        <article>
+        <article id="detail-card">
+            arrow left svg
+            pokeball svg
+            chevron left svg
+            chevron right svg
             <h2>${capitalizedName}</h2>
-            <p>"#"${dexNumber.padStart(4, "0")}</p>
-            <img src="${spriteUrl}${dexNumber}.png">
-            <div>
+            <p>"#"${dexNumber.padStart(3, "0")}</p>
+            <figure>
+                <img src="${artworkUrl}${dexNumber}.png">
+            </figure>
+            <section>
                 <p>${types[0]}</p>
                 <p>${types[1]}</p>
-            </div>
-            <section>
                 <h3>About</h3>
                 <div>
                     <svg>
@@ -46,19 +69,18 @@ function displayPokemonDetails(data) {
                     <p>Abilities</p>
                 </div>
             </section>
-            <p>Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.</p>
+            <p>${flavor_text[0]}</p>
             <h3>Base Stats</h3>
             <ul>
-                <li>HP <span> </span> ${stats[0]}</li>
-                <li>ATK <span> </span> ${stats[1]}</li>
-                <li>DEF <span> </span> ${stats[2]}</li>
-                <li>SATK <span> </span> ${stats[3]}</li>
-                <li>SDEF <span> </span> ${stats[4]}</li>
-                <li>SPD <span> </span> ${stats[5]}</li>
+                <li>HP <span> | </span> ${stats[0]} <progress value="${stats[0]}" max="200"></li>
+                <li>ATK <span> | </span> ${stats[1]}</li>
+                <li>DEF <span> | </span> ${stats[2]}</li>
+                <li>SATK <span> | </span> ${stats[3]}</li>
+                <li>SDEF <span> | </span> ${stats[4]}</li>
+                <li>SPD <span> | </span> ${stats[5]} <div id="progress-bar"><div id="progress-line"></div></div></li>
             </ul>
-        </article>
-        `
-    }).join("");
+        </article> `
+    }).join("")
     mainDom.insertAdjacentHTML("beforeend", pokemonString)
 }
 
@@ -67,4 +89,3 @@ function getPokemonId(pokemonUrl) {
 }
 
 fetchPokemon()
-console.log(pokemonString);
--- dex.js
+++ dex.js
@@ -1,4 +1,22 @@
-const searchBar = document.querySelector("#search-bar")
+import { pokeballSmall, searchIcon, xIcon, letter, tag } from "./icons.js"
+
+const pageSearch = window.location.search;
+console.log(pageSearch);
+const params = new URLSearchParams(pageSearch);
+console.log(params);
+let id = params.get("id")
+console.log(id);
+const baseUrl = `https://pokeapi.co/api/v2/pokemon/`
+console.log(baseUrl);
+
+const bodyWrapperDom = document.querySelector("#body-wrapper")
+const mainDom = document.querySelector("main")
+let offset = 0
+let limit = 20
+const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+let pokemons = []
+
+/* const searchBar = document.querySelector("#search-bar")
 const searchX = document.querySelector("#search-x")
 searchBar.addEventListener("input", showX)
 searchX.addEventListener("click", clearInput)
@@ -13,13 +31,59 @@ function clearInput() {
         searchX.classList.remove("visible")
         searchX.classList.add("invisible")
         searchBar.value = ""
+        fetchPokemon()
+        return
     }
+} */
+
+async function init() {
+    const numberOfPokemon = 1350
+    const res = await fetch(`${baseUrl}?limit=${numberOfPokemon}`)
+    const data = await res.json()
+    pokemons = data.results
+    displayHeader()
+    const searchBar = document.querySelector("#search-bar")
+    searchPokemon(searchBar)
 }
 
-const mainDom = document.querySelector("main")
-let offset = 0
-let limit = 20
-const spriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+function searchPokemon(searchBar) {
+    searchBar.addEventListener("input", (event) => {
+        if(searchBar.value === "") {
+            fetchPokemon()
+        }
+        const inputValue = event.target.value.toLowerCase()
+        runSearch(inputValue)
+    })
+}
+
+function runSearch(inputValue) {
+    const value = inputValue.trim()
+    if(!value) {
+        mainDom.innerHTML = ""
+        return
+    }
+    let pokemonSearchArray
+    pokemonSearchArray = searchByName(pokemons, value)
+    displaySearch(pokemonSearchArray)
+}
+
+function displaySearch(data) {
+    mainDom.innerHTML = ""
+    const pokemons = data
+    .map((pokemon) => {
+        const pokemonIndex = getPokemonId(pokemon.url)
+        const basePath = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/"
+        const imgPath = `${basePath}${pokemonIndex}.png`
+        const searchTemplate = /* html */ `
+        <figure class="pokemon">
+        <span class=pokemon-number">${formatDexNumber(pokemonIndex)}</span>
+        <img src="${imgPath}" alt="${pokemon.name}>"
+        <a href="details.html?id=${pokemonIndex}?url=${pokemon.url}&name=${pokemon.name}" class="pokemon-link"></a>
+        </figure>`
+        return searchTemplate
+    }).join("")
+    mainDom.insertAdjacentHTML("beforeend", pokemons)
+}
 
 function fetchPokemon() {
     fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
@@ -27,6 +91,29 @@ function fetchPokemon() {
         .then((data) => {
             displayPokemon(data)
     })
+}
+
+function displayHeader() {
+    const headerDom = document.querySelector("#body-header")
+    if(headerDom) {
+        headerDom.remove()
+    }
+    const header = /* html */ `
+    <header id="body-header">
+    <section>
+        <h1>Pokédex</h1>
+        <span class="pokeball">${pokeballSmall}</span>
+    </section>
+    <nav>
+        <span id="search-glass">${searchIcon}</span>
+        <input id="search-bar" type="search" placeholder="Search">
+        <span id="search-x" class="invisible">${xIcon}</span>
+        <button>
+            <span id="sort">${letter}</svg>
+        </button>
+    </nav> 
+    </header>`
+    bodyWrapperDom.insertAdjacentHTML("afterbegin", header)
 }
 
 function displayPokemon(data) {
@@ -38,14 +125,13 @@ function displayPokemon(data) {
             name.charAt(0).toUpperCase()
             + name.slice(1)
         return /*html*/ `
-        <article>
+        <article class="pokemon-card">
             <p>#${dexNumber.padStart(3, "0")}</p>
             <h2>${capitalizedName}</h2>
-            <img src="${spriteUrl}${dexNumber}.png">
-            <a href="details.html"></a>
-        </article>
-        `
-    }).join("");
+            <img src="${artworkUrl}${dexNumber}.png" alt="${capitalizedName}">
+            <a href="details.html?id=${dexNumber}?url=${url}&name=${name}"></a>
+        </article>`
+    }).join("")
     mainDom.insertAdjacentHTML("beforeend", pokemonString)
     let observedPokemon = document.querySelector("main article:nth-last-child(5)")
     observer.observe(observedPokemon)
@@ -53,6 +139,16 @@ function displayPokemon(data) {
 
 function getPokemonId(pokemonUrl) {
     return pokemonUrl.slice(0, -1).split("/").pop()
+}
+
+function formatDexNumber(id) {
+    return "#" + String(id.padStart(3, "0"))
+}
+
+function searchByName(pokemonArray, letter) {
+    return pokemonArray.filter((pokemon) =>
+        pokemon.name.includes(letter.toLowerCase())
+    )
 }
 
 const observer = new IntersectionObserver((entries) => {
@@ -70,28 +166,5 @@ const observer = new IntersectionObserver((entries) => {
     threshold: 1
 })
 
+init()
 fetchPokemon(offset)
-
-/* const articleDom = document.querySelectorAll("article")
-articleDom.addEventListener("click", goToDetails)
-
-articleDom.forEach((article) => {
-    article.setAttribute("href", `detaljer.html?id=${id}`)
-    article.addEventListener
-}) */
-
-/* pokemon artwork fetch from??
-"sprites": {
-    "back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
-    "back_female": null,
-    "back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
-    "back_shiny_female": null,
-    "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-    "front_female": null,
-    "front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
-    "front_shiny_female": null,
-    "other": {
-        "dream_world": {
-            "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
-            "front_female": null
-} */
